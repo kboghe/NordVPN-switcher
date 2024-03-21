87a4ba22c8ab4eef4b913dc11ebea8158c4d265f
ualers <125712906+ualers@users.noreply.github.com>
2024-03-21 10:46:00 -0300

    Update new_server_end_point.py

--- new_server_end_point.py
+++ new_server_end_point.py
@@ -1,13 +1,17 @@
 def get_nordvpn_servers():
-    serverlist =  BeautifulSoup(requests.get("https://nordvpn.com/api/server").content,"html.parser")
+    serverlist =  BeautifulSoup(requests.get("https://api.nordvpn.com/v1/servers").content,"html.parser")
     site_json=json.loads(serverlist.text)
-
-    filtered_servers = {key: [] for key in ['windows_names','linux_names']}
+    filtered_servers = {'windows_names': [], 'linux_names': []}
+    
     for specific_dict in site_json:
         try:
-            if specific_dict['categories'][0]['name'] == 'Standard VPN servers':
-                filtered_servers['windows_names'].append(specific_dict['name'])
-                filtered_servers['linux_names'].append(specific_dict['domain'].split('.')[0])
-        except IndexError:
-            pass
+            groups = specific_dict.get('groups', [])  # Verifica se 'groups' está presente
+            for group in groups:
+                if group['title'] == 'Standard VPN servers':
+                    filtered_servers['windows_names'].append(specific_dict['name'])
+                    filtered_servers['linux_names'].append(specific_dict['domain'].split('.')[0])
+                    break  # Sai do loop de grupos se encontrar a categoria desejada
+        except KeyError:
+            pass  # Ignora dicionários que não possuem a chave 'groups' ou 'title'
+
     return filtered_servers
